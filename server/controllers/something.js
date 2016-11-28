25aa1ce8b6adcd2957bce03ca162c5f774f0fb5a
riyanat.shittu <riyanat.shittu@ft.com>
2016-11-28 13:50:01 +0000

    Wireframe/Layout for dashboard

---
+++ server/controllers/something.js
@@ -0,0 +1,10 @@
+import createSomething from '../pages/createSomething';
+
+const maxAge = 1800; // 30 mins
+const sMaxAge = 600; // 10 min
+const cacheControl = `public, max-age=${maxAge}, s-maxage=${sMaxAge}`;
+
+export default async (req, res) => {
+  res.setHeader('Cache-Control', cacheControl);
+  res.render('something.html', await createSomething());
+};
