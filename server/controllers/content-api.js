9dd4e8e76d76cdc59d7ad4e0874089f620ebb452
Dan Murphy <dan.murphy@ft.com>
2016-11-28 13:54:45 +0000

    put in correct code for Apple in mock

--- server/controllers/content-api.js
+++ server/controllers/content-api.js
@@ -4,20 +4,35 @@ const cacheControl = `public, max-age=${maxAge}, s-maxage=${sMaxAge}`;
 
 import fetch from 'node-fetch';
 
-export default async (req, res) => {
+const base="https://api.ft.com";
+const concept=`${base}/content?isAnnotatedBy=`;
+const content=`${base}/content/`;
+const enriched=`${base}/enrichedcontent/`;
+
+export default (req, res) => {
   res.setHeader('Cache-Control', cacheControl);
 
   const tickerId = req.params.tickerId;
 
 	const mock ={
-			comanyName: "Apple Inc",
-			ticker: "APPL-NYSE",
+			companyName: "Apple Inc",
+			ticker: "AAPL-NAS",
 			articles: [
 				{
-					uuid: "2286616a-1a37-11e3-b3da-00144feab7de",
-					title: "Strong earnings send FTSE higher"
+					uuid: "0aa1a704-0bfd-30d6-9760-a3e016f97aa2",
+					title: "Stocks mixed as Trump rally loses some of its momentum",
+					publishedDate: "2016-11-15T16:00:02.000Z"
 				}
 			]
 		};
+	lookupUPPID(tickerId)
+	.then((uuid)=>{
+		console.log(`${tickerId} -> ${uuid}`);
+		return {};
+	})
   res.json(mock);
 };
+
+function lookupUPPID(tickerId) {
+	return Promise.resolve('2384fa7a-d514-3d6a-a0ea-3a711f66d0d8');
+}
