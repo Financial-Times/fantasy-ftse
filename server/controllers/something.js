ae809b951d55ba56f6951c8af0c9eba22c3b5da8
Joanna Kao <joannaskao@gmail.com>
2016-11-28 09:06:16 -0500

    rename something -> layout. extend dashboard/tickers using layout

--- server/controllers/something.js
+++ server/controllers/something.js
@@ -6,5 +6,5 @@ const cacheControl = `public, max-age=${maxAge}, s-maxage=${sMaxAge}`;
 
 export default async (req, res) => {
   res.setHeader('Cache-Control', cacheControl);
-  res.render('something.html', await createSomething());
+  res.render('layout.html', await createSomething());
 };
