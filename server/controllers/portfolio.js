4cdc805493b6b6458ba594d6ec7906c87fd8bc02
Dan Murphy <dan.murphy@ft.com>
2016-11-29 12:25:58 +0000

    Get now updates portfolio value using current stock prices, which probably won't change but who knows

--- server/controllers/portfolio.js
+++ server/controllers/portfolio.js
@@ -15,7 +15,8 @@ const newPortfolio = {
 	cash : 10000.00,
 	holdings: {},
 	rank: 0,
-	history : []
+	history : [],
+	lastUpdated: null
 };
 
 const newHolding = {
@@ -59,10 +60,13 @@ function read(req, res) {
 	getConnection().then((db)=>{
 		db.collection('portfolios').findOne({id})
 		.then((r)=>{
-			console.log(r);
-			delete r["_id"];
-			db.close();
-			res.status(201).json(r);
+			// console.log(r);
+			update(r)
+			.then((ur)=>{
+				delete r["_id"];
+				db.close();
+				res.status(201).json(ur);
+			});
 		});
 	})
 	.catch((err)=>{
@@ -70,20 +74,48 @@ function read(req, res) {
 	});
 }
 
+function update(portfolio) {
+	var holdings = (portfolio.holdings);
+	var totalValue = 0;
+	var updates = Object.keys(holdings).map((key) => {
+		return fetch(`http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=${key}`)
+		.then ((apiRes) => {
+			return apiRes.json();
+		})
+		.then((quote)=>{
+			var value = quote.LastPrice * holdings[key].quantity;
+			totalValue = totalValue + value;
+		});
+	});
+	return Promise.all(updates)
+	.then((res) => {
+		// console.log(totalValue);
+		return getConnection().then((db)=>{
+			portfolio.lastUpdated = new Date().toJSON();
+			portfolio.value = totalValue;
+			return db.collection('portfolios').updateOne({id:portfolio.id}, {$set:{value:portfolio.value, lastUpdated:portfolio.lastUpdated}})
+			.then((result)=>{
+				// console.log(result);
+				return portfolio;
+			});
+		});
+	});
+}
+
 function addStockInfo(req, res) {
 	var stock=req.body.tickerId;
 	return fetch(`http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=${stock}`)
 	.then ((apiRes) => {
-		console.log(apiRes.status);
+		// console.log(apiRes.status);
 		return apiRes.json();
 	})
 	.then((mdResp) => {
-		console.log(mdResp);
+		// console.log(mdResp);
 		if (mdResp.Status === "SUCCESS") {
-			console.log(req.body);
+			// console.log(req.body);
 			req.body.price = mdResp.LastPrice;
 			req.body.stockName = mdResp.Name;
-			console.log(req.body);
+			// console.log(req.body);
 			return Promise.resolve(mdResp.Status);
 		} else {
 			return Promise.reject(mdResp.Status);
@@ -119,7 +151,7 @@ function performTrade(req, res, isBuy) {
 	var stockName = req.body.stockName;
 	var quantity=req.body.quantity;
 	var price = req.body.price;
-	console.log(`performTrade: ${isBuy?"Buy":"Sell"} ${quantity} of ${stock} @ ${price}`);
+	// console.log(`performTrade: ${isBuy?"Buy":"Sell"} ${quantity} of ${stock} @ ${price}`);
 	var db;
 	return getConnection()
 	.then((d)=>{
@@ -127,19 +159,32 @@ function performTrade(req, res, isBuy) {
 		return d.collection('portfolios').findOne({id:req.get("userId")});
 	})
 	.then((r)=>{
-		console.log(r);
+		// console.log(r);
 		var tradeValue = price*quantity;
+		var cp = r.holdings[stock];
+		var currVolume = 0;
+		if (cp) {
+			currVolume = cp.quantity;
+		}
+		console.log(`current number of ${stockName} ${currVolume}`);
 		if (isBuy && tradeValue >= r.cash ) {
 			delete r["_id"];
-			req.status(402).json({err: "NotEnoughDosh", message: `You have ${r.cash}, stock ${price}*${quantity}=${tradeValue}. You are short by ${tradeValue-r.cash}`});
+			res.status(402).json({err: "NotEnoughDosh", message: `You have ${r.cash}, stock ${price}*${quantity}=${tradeValue}. You are short by ${tradeValue-r.cash}`});
+		} else if (!isBuy && quantity > currVolume ) {
+			delete r["_id"];
+			res.status(402).json({err: "NotEnoughStock", message: `You don't have ${quantity} of ${stockName}. Please don't try to cheat!`});
 		} else {
-			console.log(`${isBuy?"Buying":"Selling"} ${quantity} of ${stock} @ ${price}`);
+			// console.log(`${isBuy?"Buying":"Selling"} ${quantity} of ${stock} @ ${price}`);
 			var history = Object.assign({}, newHistory);
 			history.date = new Date().toJSON();
-			history.portfolio =  { value: r.value, cash: r.cash, holdings: r.holdings, rank: r.rank };
-			console.log(history);
+			var oldHoldings = {};
+			Object.keys(r.holdings).forEach((key) => {
+				oldHoldings[key] = Object.assign({}, r.holdings[key]);
+			});
+			history.portfolio =  { value: r.value, cash: r.cash, holdings: oldHoldings, rank: r.rank };
+			// console.log(history);
 			r.history.push(history);
-			console.log(r);
+			// console.log(r);
 			if (isBuy) {
 				r.value = r.value + tradeValue;
 				r.cash = r.cash - tradeValue;
@@ -155,27 +200,27 @@ function performTrade(req, res, isBuy) {
 								holding.id = stock;
 								holding.name = stockName;
 			}
-			console.log(holding);
+			// console.log(holding);
 			if (isBuy) {
 				holding.quantity = holding.quantity + quantity;
-				r.cash = r.cash - tradeValue;
+				// r.cash = r.cash - tradeValue;
 			} else {
 				holding.quantity = holding.quantity - quantity;
-				r.cash = r.cash + tradeValue;
+				// r.cash = r.cash + tradeValue;
 			}
-			console.log(holding);
+			// console.log(holding);
 			r.holdings[stock] = holding;
-			console.log(r.holdings);
-			console.log("Update db");
+			// console.log(r.holdings);
+			// console.log("Update db");
 			// {holdings: r.holdings, value: r.value, cash: r.cash}
 			return db.collection('portfolios').updateOne({id:r.id}, {$set: r})
 			.then((result)=>{
-				console.log(result);
+				// console.log(result);
 				db.close();
 				delete r["_id"];
 				res.status(200).json(r);
 			}).catch((err) => {
-				console.log(err);
+				// console.log(err);
 				db.close();
 				res.status(500).json(err);
 			});
@@ -183,5 +228,6 @@ function performTrade(req, res, isBuy) {
 	})
 	.catch((err)=>{
 		console.log(err);
+		throw err;
 	});
 }
